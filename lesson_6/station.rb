5c6cb4e2005d8e4d69a5986d2b5c70bdb1f903f8
alex <nuum11@mail.ru>
2022-03-24 17:15:08 +0300

    add lesson_6

---
+++ lesson_6/station.rb
@@ -0,0 +1,41 @@
+class Station
+  attr_reader :name, :list_of_train
+
+  include InstanceCounter
+  include Valid
+
+  @@stations = []
+
+  FORMAT_NAME = /^[a-z0-9]+_?[a-z0-9]+$/i
+
+  def self.all
+    @@stations
+  end
+
+  def initialize(name)
+    @name = name
+    @list_of_train = []
+    @@stations << self
+    validate!
+    register_instance
+  end
+
+  def take_train(train)
+    list_of_train << train
+  end
+
+  def send_train(train)
+    list_of_train.delete(train)
+  end
+
+  def type_of_trains(type)
+    list_of_train.select { |train| train.type == type }
+  end
+
+private
+
+  def validate!
+    raise "Name is wrong" if name !~ FORMAT_NAME
+    raise "Name must be no longer than 20 letters" if name.length > 20
+  end
+end
